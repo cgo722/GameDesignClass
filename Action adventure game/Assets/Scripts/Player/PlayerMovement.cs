f6efe6ffdc714c968fa0a93cf403869447d04458
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-10-01 17:16:50 -0600

    Run and jump script and project

---
+++ Action adventure game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float runSpeed;
+    public float runRotateSpeed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var vInput = Input.GetAxis("Vertical");
+        var hInput = Input.GetAxis("Horizontal");
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.forward * vInput * runSpeed * Time.deltaTime);
+            transform.Rotate(0, hInput * runRotateSpeed, 0);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * vInput * moveSpeed * Time.deltaTime);
+            transform.Rotate(0, hInput * rotateSpeed, 0);
+        }
+    }
+}
