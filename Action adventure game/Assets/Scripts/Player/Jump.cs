81802ab72ed943b3770d36c67d215dc5a59a39c7
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-10-05 16:30:14 -0600

    Programmed my jump script and added bolt to unity

---
+++ Action adventure game/Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+
+    public float jumpHeight;
+
+    public int jumpCountMax = 0;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCountMax < 2)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+            jumpCountMax++;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCountMax = 0;            
+        }
+    }
+
+}
--- Action adventure game/Assets/Scripts/Player/PlayerMovement.cs
+++ Action adventure game/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,12 +10,18 @@ public class PlayerMovement : MonoBehaviour
     public float rotateSpeed;
     public float runSpeed;
     public float runRotateSpeed;
+    public float jumpHeight;
+
+    private int jumpCountMax = 0;
 
     private Rigidbody rb;
+
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
     }
 
     void Update()
@@ -33,5 +39,27 @@ public class PlayerMovement : MonoBehaviour
             transform.Translate(Vector3.forward * vInput * moveSpeed * Time.deltaTime);
             transform.Rotate(0, hInput * rotateSpeed, 0);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCountMax < 2)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+            jumpCountMax++;
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var explosionForce = 5000;
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCountMax = 0;            
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            rb.AddForce(force * explosionForce, ForceMode.Impulse);
+        }
+    }
+
 }
