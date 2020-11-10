f5f085da1c683ac7b41f28637a2ba6255faeedea
Camron Oakeson <holy_hades@ymail.com>
2020-11-09 21:04:36 -0700

    Still trying to figure out why navmesh wont work and I got the moving platform script to work

--- Action adventure game/Assets/Scripts/OtherMechanics/Moving Platforms/SlidingPlatforms.cs
+++ Action adventure game/Assets/Scripts/OtherMechanics/Moving Platforms/SlidingPlatforms.cs
@@ -2,11 +2,14 @@
 
 public class SlidingPlatforms : MonoBehaviour
 {
-    public Vector3Data position1;
-    public Vector3Data position2;
+    public Vector3 position1;
+    public Vector3 position2;
+
+    public float speed;
+    public float holdTime;
 
     private void Update()
     {
-        transform.position = Vector3.Lerp(position1.value, position2.value, Mathf.PingPong(Time.time, 1));
+        transform.position = Vector3.Lerp(position1, position2, Mathf.PingPong(Time.time * speed, holdTime));
     }
 }
